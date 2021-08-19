c97aab6e93c64c1cf26680da9f5ee31d210d125f
Loitp <v.loitp@cloudhms.net>
2021-08-19 22:41:29 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/analytics/interceptors/RestRequestInterceptor.kt
+++ uzplayer/src/main/java/com/uiza/sdk/analytics/interceptors/RestRequestInterceptor.kt
@@ -1,23 +1,21 @@
-package com.uiza.sdk.analytics.interceptors;
+package com.uiza.sdk.analytics.interceptors
 
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+import kotlin.Throws
 
-import androidx.annotation.NonNull;
+class RestRequestInterceptor : Interceptor {
 
-import java.io.IOException;
-
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
-
-public class RestRequestInterceptor implements Interceptor {
-
-    private static final String CONTENT_TYPE = "application/json";
-
-    @NonNull
-    @Override
-    public Response intercept(Chain chain) throws IOException {
-        Request.Builder builder = chain.request().newBuilder();
-        builder.addHeader("Content-Type", CONTENT_TYPE);
-        return chain.proceed(builder.build());
+    companion object {
+        private const val CONTENT_TYPE = "application/json"
     }
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+        builder.addHeader("Content-Type", CONTENT_TYPE)
+        return chain.proceed(builder.build())
+    }
+
 }
