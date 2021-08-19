51fccf44cedfdb3bbd5010b9afd25fd202a3ec2b
Loitp <v.loitp@cloudhms.net>
2021-08-19 22:39:49 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/analytics/helps/JsonDateSerializer.kt
+++ uzplayer/src/main/java/com/uiza/sdk/analytics/helps/JsonDateSerializer.kt
@@ -1,22 +1,24 @@
-package com.uiza.sdk.analytics.helps;
+package com.uiza.sdk.analytics.helps
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.JsonSerializer
+import kotlin.Throws
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import org.joda.time.DateTime
+import java.io.IOException
+import java.util.*
 
-import org.joda.time.DateTime;
+class JsonDateSerializer : JsonSerializer<Date?>() {
 
-import java.io.IOException;
-import java.util.Date;
+    companion object {
+        private const val DATE_TIME_FMT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    }
 
-public class JsonDateSerializer extends JsonSerializer<Date> {
-
-    private static final String DATE_TIME_FMT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-
-    @Override
-    public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+    @Throws(IOException::class)
+    override fun serialize(value: Date?, gen: JsonGenerator, serializers: SerializerProvider) {
         if (value != null) {
-            gen.writeString((new DateTime(value.getTime())).toString(DATE_TIME_FMT));
+            gen.writeString(DateTime(value.time).toString(DATE_TIME_FMT))
         }
     }
+
 }
