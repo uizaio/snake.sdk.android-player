25739c1f0226ab72697f1d6e88ef6050b74b1c6e
Loitp <v.loitp@cloudhms.net>
2021-08-20 13:59:52 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/listerner/UZTVFocusChangeListener.kt
+++ uzplayer/src/main/java/com/uiza/sdk/listerner/UZTVFocusChangeListener.kt
@@ -1,7 +1,7 @@
-package com.uiza.sdk.listerner;
+package com.uiza.sdk.listerner
 
-import android.view.View;
+import android.view.View
 
-public interface UZTVFocusChangeListener {
-    void onFocusChange(View view, boolean isFocus);
+interface UZTVFocusChangeListener {
+    fun onFocusChange(view: View?, isFocus: Boolean)
 }
