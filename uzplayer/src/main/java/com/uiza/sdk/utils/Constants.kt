b0764fcf827a7bb4db823f878172bfe45dd68ba0
Loitp <v.loitp@cloudhms.net>
2021-08-20 14:54:25 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/utils/Constants.kt
+++ uzplayer/src/main/java/com/uiza/sdk/utils/Constants.kt
@@ -1,37 +1,27 @@
-package com.uiza.sdk.utils;
+package com.uiza.sdk.utils
 
-import com.uiza.sdk.BuildConfig;
-
-public class Constants {
-
-    public static final String PLAYER_NAME = "UZPlayer";
-    public static final String PLAYER_SDK_VERSION = "1.0.1";
-
-    public static final float RATIO_9_16 = 9f / 16f;
-    public static final float RATIO_10_16 = 10f / 16f;
-    public static final float RATIO_11_16 = 11f / 16f;
-    public static final float RATIO_12_16 = 12f / 16f;
-    public static final float RATIO_16_16 = 1;
-
-    public static final int RATIO_LAND_TABLET = 24;
-    public static final int RATIO_PORTRAIT_TABLET = 20;
-    public static final int RATIO_LAND_MOBILE = 18;
-    public static final int RATIO_PORTRAIT_MOBILE = 12;
-
-    public static final int W_320 = 320;
-    public static final int W_180 = 180;
-
-    public static final boolean DF_PLAYER_IS_AUTO_START = true;
-    public static final String TEXT_TYPE = "text/plain";
-    public static final String DRM_LICENSE_URL = "";
-    public final static String KEY_UIZA_ENTITY_ID = "KEY_UIZA_ENTITY_ID";
-    public static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 6969;
-
-    public final static String FLOAT_LINK_PLAY = "FLOAT_LINK_PLAY";
-    public final static String FLOAT_CONTENT_POSITION = "FLOAT_CONTENT_POSITION";
-    public final static String FLOAT_IS_LIVESTREAM = "FLOAT_IS_LIVESTREAM";
-    public final static String FLOAT_CONTROL_SKIN_ID = "FLOAT_CONTROL_SKIN_ID";
-
-    public final static String URL_IMG_THUMBNAIL_BLACK = "https://static.uiza.io/black_1px.jpg";
-
+object Constants {
+    const val PLAYER_NAME = "SnakePlayer"
+    const val PLAYER_SDK_VERSION = "1.0.1"
+    const val RATIO_9_16 = 9f / 16f
+    const val RATIO_10_16 = 10f / 16f
+    const val RATIO_11_16 = 11f / 16f
+    const val RATIO_12_16 = 12f / 16f
+    const val RATIO_16_16 = 1f
+    const val RATIO_LAND_TABLET = 24
+    const val RATIO_PORTRAIT_TABLET = 20
+    const val RATIO_LAND_MOBILE = 18
+    const val RATIO_PORTRAIT_MOBILE = 12
+    const val W_320 = 320
+    const val W_180 = 180
+    const val DF_PLAYER_IS_AUTO_START = true
+    const val TEXT_TYPE = "text/plain"
+    const val DRM_LICENSE_URL = ""
+    const val KEY_UIZA_ENTITY_ID = "KEY_UIZA_ENTITY_ID"
+    const val CODE_DRAW_OVER_OTHER_APP_PERMISSION = 6969
+    const val FLOAT_LINK_PLAY = "FLOAT_LINK_PLAY"
+    const val FLOAT_CONTENT_POSITION = "FLOAT_CONTENT_POSITION"
+    const val FLOAT_IS_LIVESTREAM = "FLOAT_IS_LIVESTREAM"
+    const val FLOAT_CONTROL_SKIN_ID = "FLOAT_CONTROL_SKIN_ID"
+    const val URL_IMG_THUMBNAIL_BLACK = "https://static.uiza.io/black_1px.jpg"
 }
