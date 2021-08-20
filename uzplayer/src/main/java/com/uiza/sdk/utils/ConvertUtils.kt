0bb64301a9e3264b7e540d0214d03f21522d6edd
Loitp <v.loitp@cloudhms.net>
2021-08-20 14:57:07 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/utils/ConvertUtils.kt
+++ uzplayer/src/main/java/com/uiza/sdk/utils/ConvertUtils.kt
@@ -1,93 +1,94 @@
-package com.uiza.sdk.utils;
+package com.uiza.sdk.utils
 
-import android.content.res.Resources;
-import android.text.TextUtils;
+import android.content.res.Resources
+import android.text.TextUtils
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.source.hls.playlist.HlsMasterPlaylist
+import com.google.android.exoplayer2.source.hls.playlist.HlsMediaPlaylist
 
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.source.hls.playlist.HlsMasterPlaylist;
-import com.google.android.exoplayer2.source.hls.playlist.HlsMediaPlaylist;
+object ConvertUtils {
+    private const val EXT_X_PROGRAM_DATE_TIME = "#EXT-X-PROGRAM-DATE-TIME:"
+    private const val EXTINF = "#EXTINF:"
+    private const val EXT_X_UZ_TIMESHIFT = "#EXT-X-UZ-TIMESHIFT:"
 
-public class ConvertUtils {
-
-    private static final String EXT_X_PROGRAM_DATE_TIME = "#EXT-X-PROGRAM-DATE-TIME:";
-    private static final String EXTINF = "#EXTINF:";
-    private static final String EXT_X_UZ_TIMESHIFT = "#EXT-X-UZ-TIMESHIFT:";
-
-    private ConvertUtils() {
-        throw new UnsupportedOperationException("u can't instantiate me...");
+    @JvmStatic
+    fun dp2px(dpValue: Float): Int {
+        val scale = Resources.getSystem().displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
     }
 
-    public static int dp2px(float dpValue) {
-        final float scale = Resources.getSystem().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+    fun px2dp(pxValue: Float): Int {
+        val scale = Resources.getSystem().displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
     }
 
-    public static int px2dp(float pxValue) {
-        final float scale = Resources.getSystem().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
+    fun sp2px(spValue: Float): Int {
+        val fontScale = Resources.getSystem().displayMetrics.scaledDensity
+        return (spValue * fontScale + 0.5f).toInt()
     }
 
-    public static int sp2px(float spValue) {
-        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
-        return (int) (spValue * fontScale + 0.5f);
+    fun px2sp(pxValue: Float): Int {
+        val fontScale = Resources.getSystem().displayMetrics.scaledDensity
+        return (pxValue / fontScale + 0.5f).toInt()
     }
 
-    public static int px2sp(float pxValue) {
-        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
-        return (int) (pxValue / fontScale + 0.5f);
-    }
-
-    public static String getTimeShiftUrl(HlsMasterPlaylist playlist) {
-        if (playlist == null || ListUtils.isEmpty(playlist.tags))
-            return null;
-        for (String tag : playlist.tags) {
+    @JvmStatic
+    fun getTimeShiftUrl(playlist: HlsMasterPlaylist?): String? {
+        if (playlist == null || ListUtils.isEmpty(playlist.tags)) return null
+        for (tag in playlist.tags) {
             if (tag.contains(EXT_X_UZ_TIMESHIFT)) {
-                return tag.replace(EXT_X_UZ_TIMESHIFT, "");
+                return tag.replace(EXT_X_UZ_TIMESHIFT, "")
             }
         }
-        return null;
+        return null
     }
 
-    public static long getProgramDateTime(HlsMediaPlaylist playlist, long timeToEndChunk) {
-        if (playlist == null || ListUtils.isEmpty(playlist.tags))
-            return C.INDEX_UNSET;
-        final String emptyStr = "";
-        final int tagSize = playlist.tags.size();
-        long totalTime = 0;
-        int playingIndex = tagSize;
+    @JvmStatic
+    fun getProgramDateTime(playlist: HlsMediaPlaylist?, timeToEndChunk: Long): Long {
+        if (playlist == null || ListUtils.isEmpty(playlist.tags)) return C.INDEX_UNSET.toLong()
+        val emptyStr = ""
+        val tagSize = playlist.tags.size
+        var totalTime: Long = 0
+        var playingIndex = tagSize
         // Find the playing frame index
         while (playingIndex > 0) {
-            String tag = playlist.tags.get(playingIndex - 1);
+            val tag = playlist.tags[playingIndex - 1]
             if (tag.contains(EXTINF)) {
-                totalTime +=
-                        Double.parseDouble(tag.replace(",", emptyStr).replace(EXTINF, emptyStr)) * 1000;
+                totalTime += (tag.replace(",", emptyStr).replace(
+                    oldValue = EXTINF,
+                    newValue = emptyStr
+                )
+                    .toDouble() * 1000).toLong()
                 if (totalTime >= timeToEndChunk) {
-                    break;
+                    break
                 }
             }
-            playingIndex--;
+            playingIndex--
         }
         if (playingIndex >= tagSize) {
             // That means the livestream latency is larger than 1 segment (duration).
             // we should skip to calc latency in this case
-            return C.INDEX_UNSET;
+            return C.INDEX_UNSET.toLong()
         }
         // Find the playing frame EXT_X_PROGRAM_DATE_TIME
-        String playingDateTime = emptyStr;
-        for (int i = playingIndex; i < tagSize; i++) {
-            String tag = playlist.tags.get(i);
+        var playingDateTime = emptyStr
+        for (i in playingIndex until tagSize) {
+            val tag = playlist.tags[i]
             if (tag.contains(EXT_X_PROGRAM_DATE_TIME)) {
-                playingDateTime = tag.replace(EXT_X_PROGRAM_DATE_TIME, emptyStr);
-                break;
+                playingDateTime = tag.replace(
+                    oldValue = EXT_X_PROGRAM_DATE_TIME,
+                    newValue = emptyStr
+                )
+                break
             }
         }
-
-        if (TextUtils.isEmpty(playingDateTime)) {
+        return if (TextUtils.isEmpty(playingDateTime)) {
             // That means something wrong with the format, check with server
             // we should skip to calc latency in this case
-            return C.INDEX_UNSET;
+            C.INDEX_UNSET.toLong()
+        } else {
+            StringUtils.convertUTCMs(playingDateTime)
         }
         // int list of frame, we get the EXT_X_PROGRAM_DATE_TIME of current playing frame
-        return StringUtils.convertUTCMs(playingDateTime);
     }
 }
