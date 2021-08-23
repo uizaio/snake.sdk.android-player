bfb65c29b94343a576d477d23575e592a8ef8821
Loitp <v.loitp@cloudhms.net>
2021-08-23 17:04:31 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/dialog/speed/Callback.kt
+++ uzplayer/src/main/java/com/uiza/sdk/dialog/speed/Callback.kt
@@ -1,5 +1,5 @@
-package com.uiza.sdk.dialog.speed;
+package com.uiza.sdk.dialog.speed
 
-public interface Callback {
-    void onSelectItem(Speed speed);
+interface Callback {
+    fun onSelectItem(speed: Speed)
 }
--- uzplayer/src/main/java/com/uiza/sdk/dialog/speed/Speed.kt
+++ uzplayer/src/main/java/com/uiza/sdk/dialog/speed/Speed.kt
@@ -1,27 +1,6 @@
-package com.uiza.sdk.dialog.speed;
+package com.uiza.sdk.dialog.speed
 
-public class Speed {
-    private String name;
-    private float value;
-
-    public Speed(String name, float value) {
-        this.name = name;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getValue() {
-        return value;
-    }
-
-    public void setValue(float value) {
-        this.value = value;
-    }
-}
+class Speed(
+    var name: String,
+    var value: Float
+)
