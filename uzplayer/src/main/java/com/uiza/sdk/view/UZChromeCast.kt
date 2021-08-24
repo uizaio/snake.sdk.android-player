6a6666b76674be5dd02d091d3e36fd86e3718634
Loitp <v.loitp@cloudhms.net>
2021-08-24 20:24:34 +0700

    update

--- uzplayer/src/main/java/com/uiza/sdk/view/UZChromeCast.kt
+++ uzplayer/src/main/java/com/uiza/sdk/view/UZChromeCast.kt
@@ -1,95 +1,101 @@
-package com.uiza.sdk.view;
+package com.uiza.sdk.view
 
-import android.content.Context;
-import android.view.View;
+import android.content.Context
+import android.view.View
+import androidx.annotation.UiThread
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.cast.framework.CastState
+import com.google.android.gms.cast.framework.CastStateListener
+import com.uiza.sdk.chromecast.Casty.OnConnectChangeListener
+import com.uiza.sdk.exceptions.ErrorConstant
+import com.uiza.sdk.listerner.UZChromeCastListener
+import com.uiza.sdk.utils.UZAppUtils
+import com.uiza.sdk.utils.UZData
+import com.uiza.sdk.utils.UZViewUtils
+import com.uiza.sdk.widget.UZMediaRouteButton
 
-import androidx.annotation.UiThread;
+//chromecast https://github.com/DroidsOnRoids/Casty
+class UZChromeCast {
+    var mediaRouteButton: UZMediaRouteButton? = null
+        private set
+    private var listener: UZChromeCastListener? = null
 
-import com.google.android.gms.cast.framework.CastContext;
-import com.google.android.gms.cast.framework.CastState;
-import com.uiza.sdk.chromecast.Casty;
-import com.uiza.sdk.exceptions.ErrorConstant;
-import com.uiza.sdk.listerner.UZChromeCastListener;
-import com.uiza.sdk.utils.UZAppUtils;
-import com.uiza.sdk.utils.UZData;
-import com.uiza.sdk.utils.UZViewUtils;
-import com.uiza.sdk.widget.UZMediaRouteButton;
-
-/**
- * Created by loitp on 2/27/2019.
- */
-
-public class UZChromeCast {
-
-    protected final String TAG = "TAG" + getClass().getSimpleName();
-    //chromecast https://github.com/DroidsOnRoids/Casty
-    private UZMediaRouteButton uzMediaRouteButton;
-    private UZChromeCastListener listener;
-
-    static {
-        if (!UZAppUtils.checkChromeCastAvailable())
-            throw new NoClassDefFoundError(ErrorConstant.ERR_505);
-    }
-
-    void setUZChromeCastListener(UZChromeCastListener listener) {
-        this.listener = listener;
-    }
-
-    void setupChromeCast(Context context) {
-        if (UZAppUtils.isTV(context)) return;
-        uzMediaRouteButton = new UZMediaRouteButton(context);
-        setUpMediaRouteButton();
-        addUIChromecastLayer(context);
-    }
-
-    @UiThread
-    private void setUpMediaRouteButton() {
-        Casty casty = UZData.INSTANCE.getCasty();
-        if (casty != null) {
-            casty.setUpMediaRouteButton(uzMediaRouteButton);
-            casty.setOnConnectChangeListener(new Casty.OnConnectChangeListener() {
-                @Override
-                public void onConnected() {
-                    if (listener != null) listener.onConnected();
-                }
-
-                @Override
-                public void onDisconnected() {
-                    if (listener != null) listener.onDisconnected();
-                }
-            });
+    companion object {
+        init {
+            if (!UZAppUtils.checkChromeCastAvailable()) {
+                throw NoClassDefFoundError(ErrorConstant.ERR_505)
+            }
         }
     }
 
-    private void updateMediaRouteButtonVisibility(int state) {
-        UZViewUtils.setVisibilityViews(state == CastState.NO_DEVICES_AVAILABLE ? View.GONE : View.VISIBLE, uzMediaRouteButton);
+    fun setUZChromeCastListener(listener: UZChromeCastListener?) {
+        this.listener = listener
+    }
+
+    fun setupChromeCast(context: Context) {
+        if (UZAppUtils.isTV(context)) {
+            return
+        }
+        mediaRouteButton = UZMediaRouteButton(context)
+        setUpMediaRouteButton()
+        addUIChromecastLayer(context)
+    }
+
+    @UiThread
+    private fun setUpMediaRouteButton() {
+        UZData.casty?.let {
+            mediaRouteButton?.let { bt ->
+                it.setUpMediaRouteButton(bt)
+            }
+            it.setOnConnectChangeListener(object : OnConnectChangeListener {
+                override fun onConnected() {
+                    listener?.onConnected()
+                }
+
+                override fun onDisconnected() {
+                    listener?.onDisconnected()
+                }
+            })
+        }
+    }
+
+    private fun updateMediaRouteButtonVisibility(state: Int) {
+        UZViewUtils.setVisibilityViews(
+            if (state == CastState.NO_DEVICES_AVAILABLE) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            },
+            mediaRouteButton
+        )
     }
 
     //tự tạo layout chromecast và background đen
     //Gen layout chromecast with black background programmatically
-    private void addUIChromecastLayer(Context context) {
+    private fun addUIChromecastLayer(context: Context) {
         //listener check state of chromecast
-        CastContext castContext = null;
+        var castContext: CastContext? = null
         try {
-            castContext = CastContext.getSharedInstance(context);
-        } catch (Exception e) {
-            e.printStackTrace();
+            castContext = CastContext.getSharedInstance(context)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
         if (castContext == null) {
-            UZViewUtils.goneViews(uzMediaRouteButton);
-            return;
+            UZViewUtils.goneViews(mediaRouteButton)
+            return
         }
-        updateMediaRouteButtonVisibility(castContext.getCastState());
-        castContext.addCastStateListener(this::updateMediaRouteButtonVisibility);
-        if (listener != null) listener.addUIChromeCast();
+        updateMediaRouteButtonVisibility(castContext.castState)
+        castContext.addCastStateListener(CastStateListener { state: Int ->
+            updateMediaRouteButtonVisibility(state)
+        })
+        listener?.addUIChromeCast()
     }
 
-    public void setTintMediaRouteButton(final int color) {
-        if (uzMediaRouteButton != null)
-            uzMediaRouteButton.post(() -> uzMediaRouteButton.applyTint(color));
-    }
-
-    UZMediaRouteButton getMediaRouteButton() {
-        return uzMediaRouteButton;
+    fun setTintMediaRouteButton(color: Int) {
+        mediaRouteButton?.let {
+            it.post {
+                it.applyTint(color)
+            }
+        }
     }
 }
