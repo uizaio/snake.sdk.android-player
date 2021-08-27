57f8e9389f3e3b1a1f04bee368bf1dd41a300456
Loitp <v.loitp@cloudhms.net>
2021-08-27 19:50:03 +0700

    update

--- sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerbasic/PlayerBasicActivity.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerbasic/PlayerBasicActivity.kt
@@ -2,16 +2,13 @@ package com.uiza.sampleplayer.ui.playerbasic
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.uiza.sampleplayer.R
 import com.uiza.sampleplayer.app.Constant
 import com.uiza.sampleplayer.app.UZApplication
 import com.uiza.sdk.UZPlayer
-import com.uiza.sdk.exceptions.UZException
-import com.uiza.sdk.interfaces.UZPlayerCallback
 import com.uiza.sdk.models.UZPlayback
-import com.uiza.sdk.utils.UZViewUtils
-import com.uiza.sdk.view.UZPlayerView
 import kotlinx.android.synthetic.main.activity_player_basic.*
 
 class PlayerBasicActivity : AppCompatActivity() {
@@ -28,39 +25,31 @@ class PlayerBasicActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        uzVideoView.setPlayerCallback(object : UZPlayerCallback {
-            override fun playerViewCreated(playerView: UZPlayerView) {
-                onPlay()
-            }
-
-            override fun isInitResult(linkPlay: String) {
-                log("LinkPlay $linkPlay")
-            }
-
-            override fun onTimeShiftChange(timeShiftOn: Boolean) {
-            }
-
-            override fun onScreenRotate(isLandscape: Boolean) {
-//                if (!isLandscape) {
-//                    val w = UZViewUtils.screenWidth
-//                    val h = w * 9 / 16
-//                    uzVideoView.setFreeSize(false)
-//                    uzVideoView.setSize(w, h)
-//                }
-            }
-
-            override fun onError(e: UZException) {
-                e.printStackTrace()
-            }
-        })
-
+        uzVideoView.setUseController(false)
+        btPlayVOD.setOnClickListener {
+            etLinkPlay.setText(Constant.LINK_PLAY_VOD)
+            btPlayLink.performClick()
+        }
+        btPlayLive.setOnClickListener {
+            etLinkPlay.setText(Constant.LINK_PLAY_LIVE)
+            btPlayLink.performClick()
+        }
+        btPlayLink.setOnClickListener {
+            onPlay(etLinkPlay.text.toString())
+        }
     }
 
-    private fun onPlay() {
-        val uzPlayback = UZPlayback()
-        uzPlayback.poster = UZApplication.thumbnailUrl
-        uzPlayback.addLinkPlay(Constant.LINK_PLAY_LIVE)
-        uzVideoView.play(uzPlayback)
+    private fun onPlay(link: String) {
+        if (link.isEmpty()) {
+            Toast.makeText(this, "Link play is empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (uzVideoView.isViewCreated) {
+            val uzPlayback = UZPlayback()
+            uzPlayback.poster = UZApplication.thumbnailUrl
+            uzPlayback.addLinkPlay(link)
+            uzVideoView.play(uzPlayback)
+        }
     }
 
     public override fun onDestroy() {
