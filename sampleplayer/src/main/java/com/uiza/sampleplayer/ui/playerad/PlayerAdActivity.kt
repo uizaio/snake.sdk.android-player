e77cbc871cf710ac59161dffd7f65e6e0d866d4c
Loitp <v.loitp@cloudhms.net>
2021-08-31 12:27:42 +0700

    update

--- sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerad/PlayerAdActivity.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerad/PlayerAdActivity.kt
@@ -28,6 +28,12 @@ class PlayerAdActivity : AppCompatActivity() {
                 urlIMAAd = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
             )
         }
+        btAdSingleSkippableInline.setOnClickListener {
+            onPlay(
+                link = Constant.LINK_PLAY_VOD,
+                urlIMAAd = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="
+            )
+        }
     }
 
     private fun onPlay(link: String, urlIMAAd: String) {
