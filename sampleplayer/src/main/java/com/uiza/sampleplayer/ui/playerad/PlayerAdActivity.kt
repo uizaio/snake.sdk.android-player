2cdeb5cd7fe16d4fdfe26d6a7b617cfceb4ea216
Loitp <v.loitp@cloudhms.net>
2021-08-31 12:37:29 +0700

    update

--- sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerad/PlayerAdActivity.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerad/PlayerAdActivity.kt
@@ -30,13 +30,13 @@ class PlayerAdActivity : AppCompatActivity() {
         }
         btAdSingleSkippableInline.setOnClickListener {
             onPlay(
-                link = Constant.LINK_PLAY_VOD,
+                link = Constant.LINK_PLAY_VOD_2,
                 urlIMAAd = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="
             )
         }
         btAdSingleRedirectLinear.setOnClickListener {
             onPlay(
-                link = Constant.LINK_PLAY_VOD_2,
+                link = Constant.LINK_PLAY_VOD,
                 urlIMAAd = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator="
             )
         }
