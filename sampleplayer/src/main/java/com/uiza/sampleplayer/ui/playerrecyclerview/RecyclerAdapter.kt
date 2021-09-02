596e79221069324dbcd9f7623506a7fc9c13e218
Loitp <v.loitp@cloudhms.net>
2021-09-02 21:56:30 +0700

    update

--- sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerrecyclerview/RecyclerAdapter.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerrecyclerview/RecyclerAdapter.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.uiza.sampleplayer.R
+import com.uiza.sdk.view.UZVideoView
 import kotlinx.android.synthetic.main.view_item_recycler.view.*
 
 class RecyclerAdapter(private val list: List<ItemRv>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var onClickItem: ((index: Int, itemRv: ItemRv) -> Unit)? = null
+    private val listUZVideo = ArrayList<UZVideoView>()
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         @SuppressLint("SetTextI18n")
@@ -24,15 +26,20 @@ class RecyclerAdapter(private val list: List<ItemRv>) :
             itemView.tvLinkPlay.text = "linkPlay ${itemRv.uzPlayback?.linkPlay}"
 
             if (itemRv.isPlaying) {
+                listUZVideo.forEach {
+                    it.pause()
+                    listUZVideo.remove(it)
+                }
                 itemView.linearLayout.setBackgroundColor(Color.GREEN)
                 if (itemView.uzVideoView.isViewCreated()) {
                     itemRv.uzPlayback?.let {
                         itemView.uzVideoView.play(it)
+                        listUZVideo.add(itemView.uzVideoView)
                     }
                 }
             } else {
                 itemView.linearLayout.setBackgroundColor(Color.WHITE)
-                itemView.uzVideoView.pause()
+                itemView.uzVideoView.onDestroyView()
             }
             itemView.cardView.setOnClickListener {
                 onClickItem?.invoke(adapterPosition, itemRv)
@@ -57,4 +64,10 @@ class RecyclerAdapter(private val list: List<ItemRv>) :
         }
     }
 
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        listUZVideo.forEach {
+            it.onDestroyView()
+        }
+    }
 }
