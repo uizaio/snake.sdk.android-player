26cc60c09880a7f58205fdcd6ebd08763666da27
Loitp <v.loitp@cloudhms.net>
2021-09-02 21:21:04 +0700

    update

---
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerrecyclerview/PlayerListAdapter.kt
@@ -0,0 +1,54 @@
+package com.uiza.sampleplayer.ui.playerrecyclerview
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.uiza.sampleplayer.R
+import kotlinx.android.synthetic.main.view_item_player_list.view.*
+
+class PlayerListAdapter(private val list: List<Item>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var onClickItem: ((index: Int, item: Item) -> Unit)? = null
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        @SuppressLint("SetTextI18n")
+        fun bind(item: Item) {
+            itemView.tvName.text = item.uzPlayback?.name
+            itemView.tvAd.text = "urlIMAAd: ${item.uzPlayback?.urlIMAAd}"
+            itemView.tvIsPortrait.text = "isPortraitVideo ${item.uzPlayback?.isPortraitVideo}"
+            itemView.tvPoster.text = "poster ${item.uzPlayback?.poster}"
+            itemView.tvLinkPlay.text = "linkPlay ${item.uzPlayback?.linkPlay}"
+
+            if (item.isPlaying) {
+                itemView.linearLayout.setBackgroundColor(Color.GREEN)
+            } else {
+                itemView.linearLayout.setBackgroundColor(Color.WHITE)
+            }
+            itemView.cardView.setOnClickListener {
+                onClickItem?.invoke(adapterPosition, item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_item_player_list, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is MyViewHolder) {
+            val movie = list[position]
+            holder.bind(movie)
+        }
+    }
+
+}
--- sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerrecyclerview/PlayerRecyclerViewActivity.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/ui/playerrecyclerview/PlayerRecyclerViewActivity.kt
@@ -1,71 +1,91 @@
 package com.uiza.sampleplayer.ui.playerrecyclerview
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.uiza.sampleplayer.R
 import com.uiza.sampleplayer.app.Constant
+import com.uiza.sampleplayer.ui.playerlist.Item
+import com.uiza.sampleplayer.ui.playerlist.PlayerListAdapter
 import com.uiza.sdk.models.UZPlayback
-import kotlinx.android.synthetic.main.activity_player_recycler_view.*
+import kotlinx.android.synthetic.main.activity_player_list.*
+import java.util.*
 
 class PlayerRecyclerViewActivity : AppCompatActivity() {
+    private val list: MutableList<Item> = ArrayList()
+    private var playerListAdapter: PlayerListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_recycler_view)
+
         setupViews()
+        prepareData()
     }
 
     private fun setupViews() {
-        uzVideoView.onPlayerViewCreated = {
-            uzVideoView.setAlwaysPortraitScreen(true)
-            uzVideoView.setPIPModeEnabled(false)
+        playerListAdapter = PlayerListAdapter(list)
+        playerListAdapter?.onClickItem = { index: Int, _: Item ->
         }
-        uzVideoView.onFirstStateReady = {
-            uzVideoView.setUseController(true)
-        }
-        btPlayVOD.setOnClickListener {
-            etLinkPlay.setText(Constant.LINK_PLAY_VOD)
-            btPlayLink.performClick()
-        }
-        btPlayLive.setOnClickListener {
-            etLinkPlay.setText(Constant.LINK_PLAY_LIVE)
-            btPlayLink.performClick()
-        }
-        btPlayLink.setOnClickListener {
-            onPlay(etLinkPlay.text.toString().trim())
-        }
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = playerListAdapter
     }
 
-    private fun onPlay(link: String) {
-        if (link.isEmpty()) {
-            Toast.makeText(this, "Link play is empty", Toast.LENGTH_SHORT).show()
-            return
-        }
-        if (uzVideoView.isViewCreated()) {
-            val uzPlayback = UZPlayback(linkPlay = link)
-            uzVideoView.play(uzPlayback)
-        }
+    private fun prepareData() {
+        list.add(
+            Item(
+                UZPlayback(
+                    linkPlay = Constant.LINK_PLAY_VOD,
+                    name = "VOD",
+                    isPortraitVideo = false
+                )
+            )
+        )
+        list.add(
+            Item(
+                UZPlayback(
+                    linkPlay = Constant.LINK_PLAY_LIVE,
+                    name = "LIVE",
+                    isPortraitVideo = false
+                )
+            )
+        )
+        list.add(
+            Item(
+                UZPlayback(
+                    linkPlay = Constant.LINK_PLAY_VOD_PORTRAIT,
+                    name = "Portrait",
+                    isPortraitVideo = true
+                )
+            )
+        )
+        list.add(
+            Item(
+                UZPlayback(
+                    linkPlay = Constant.LINK_PLAY_VOD,
+                    name = "Ad",
+                    isPortraitVideo = false,
+                    urlIMAAd = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
+                )
+            )
+        )
+        list.add(
+            Item(
+                UZPlayback(
+                    linkPlay = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
+                    name = "This is name of video",
+                    isPortraitVideo = false,
+                    poster = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/thumbnails/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.jpg",
+                )
+            )
+        )
+
+        notifyDataSetChanged()
     }
 
-    public override fun onDestroy() {
-        uzVideoView.onDestroyView()
-        super.onDestroy()
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        uzVideoView.onResumeView()
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        uzVideoView.onPauseView()
-    }
-
-    override fun onBackPressed() {
-        if (!uzVideoView.onBackPressed()) {
-            super.onBackPressed()
-        }
+    private fun notifyDataSetChanged() {
+        playerListAdapter?.notifyDataSetChanged()
     }
 }
