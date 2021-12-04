b70b3f15a26a13cc7b840d16b323cbf4fcd6518f
Loitp <v.loitp@cloudhms.net>
2021-12-04 19:36:42 +0700

    update size

--- sampleplayer/src/main/java/com/uiza/sampleplayer/app/UZApplication.kt
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/app/UZApplication.kt
@@ -1,15 +1,15 @@
 package com.uiza.sampleplayer.app
 
 import androidx.multidex.MultiDexApplication
+import com.uiza.sampleplayer.R
 import com.uiza.sdk.UZPlayer
 
-// TODO pip bi loi khi xoay man hinh, luc xem video portrait
 class UZApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
 
-        UZPlayer.init(com.uiza.sdk.R.layout.uzplayer_skin_default)
+        UZPlayer.init(R.layout.uzplayer_skin_default)
 //        UZPlayer.init(com.uiza.sdk.R.layout.uzplayer_skin_0)
 //        UZPlayer.init(com.uiza.sdk.R.layout.uzplayer_skin_1)
 //        UZPlayer.init(com.uiza.sdk.R.layout.uzplayer_skin_2)
